e3444ab088255439ea6c4cda8216c257b28b6bf4
FLucasF <lucasfelipegpGITHUB@gmail.com>
2024-11-21 18:31:32 -0300

    colocando money transfer

--- src/app/components/money-transfer-screen/money-transfer-screen.component.ts
+++ src/app/components/money-transfer-screen/money-transfer-screen.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrl: './money-transfer-screen.component.scss'
 })
 export class MoneyTransferScreenComponent {
+  cartItems = [
+    { name: 'Product name', description: 'Brief description', price: '$12' },
+    { name: 'Second product', description: 'Brief description', price: '$8' },
+    { name: 'Third item', description: 'Brief description', price: '$5' }
+  ];
 
+  promoCode = 'EXAMPLECODE';
+  promoInput = '';
+  promoDiscount = '$5';
+  total = '$20';
+
+  formData = {
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    address: ''
+  };
+
+  applyPromoCode(): void {
+    if (this.promoInput === this.promoCode) {
+      alert('Promo code applied!');
+    } else {
+      alert('Invalid promo code.');
+    }
+  }
+
+  onSubmit(): void {
+    alert('Form submitted!');
+    console.log(this.formData);
+  }
 }
